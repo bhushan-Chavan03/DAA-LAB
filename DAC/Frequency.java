a5a254baf4817defe5a05e1ee1e5590b5d9bf45d
Bhushan Chavan <bhushanc2003@gmail.com>
2024-05-21 12:32:12 +0530

    added frequency

---
+++ DAC/Frequency.java
@@ -0,0 +1,39 @@
+public class Frequency {
+
+
+    static int findFirstGreatest(int arr[], int num){
+        int low=0;
+        int high=arr.length-1;
+        int ans=arr.length;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]>num){
+                ans=mid;
+                high=mid-1;
+
+            }
+            else low=mid+1;
+
+
+        }
+        return ans;
+    }
+
+
+
+    static void findFreq(int arr[], int ind){
+        if(ind>=arr.length)return;
+
+        int start=findFirstGreatest(arr, arr[ind]-1);
+        int end=findFirstGreatest(arr, arr[ind]);
+        System.out.println("The frequency of"+arr[ind]+" is " + (end-start));
+        findFreq(arr, end);
+    }
+
+
+    public static void main(String [] args){
+        int arr[]={1, 2, 2 , 2, 3, 3, 3, 5 , 6 ,6 };
+        findFreq(arr,0);
+    }
+    
+}
